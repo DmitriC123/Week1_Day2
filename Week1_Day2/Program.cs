b8d8929d536da8f8fd7c253c1e17b9df03af07c3
DmitriC123 <cohan.dima@gmail.com>
2017-09-26 14:19:22 -0400

    Practiced with data types

--- Week1_Day2/Program.cs
+++ Week1_Day2/Program.cs
@@ -10,6 +10,27 @@ namespace Week1_Day2
     {
         static void Main(string[] args)
         {
+            Char firstInitial = 'D';
+
+            bool isTired = true;
+            //Boolean or bool is true or false
+
+            //Other data types
+            float number = 2.362346324632463467342343f;
+            //Remember to put 'f' at the end of every float
+            decimal partialNum = 2.362346324632463467342343m;
+            //Remember to put 'm' at the end of every decimal
+            double someNumber = 2.362346324632463467342343;
+
+            Console.WriteLine(number);
+            Console.WriteLine(partialNum);
+            Console.WriteLine(someNumber);
+
+
+
+
+
+
         }
     }
 }
